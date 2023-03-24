ac98dd7de4ea798e4e41ad5cd2204b0ec6783eaf
Hamster <denrabshit@gmail.com>
2023-03-24 22:54:55 +0200

    Initial commit

---
+++ gear_checker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+gearst=$(sudo systemctl status gear)
+
+shopt -s nocasematch
+
+if [[ $gearst =~ " active " && $gearst != *"error"* && $gearst != *"invalid"* ]]; then
+echo "Gear is ok✅"
+ else 
+echo "Check gear node❌" 
+fi
+
+shopt -u nocasematch
+
+
+1
