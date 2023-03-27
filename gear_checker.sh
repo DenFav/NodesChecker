12db09e1d2685d90b30f0c4106c04c4711bb4f31
Hamster <denrabshit@gmail.com>
2023-03-27 22:55:07 +0300

    Fixed get status code

--- gear_checker.sh
+++ gear_checker.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+get_status() {
+    STATUS=$(sudo systemctl status gear | grep -i active | awk '{print $2}')
+    echo "${STATUS}"
+}
+
 gear_status=$(get_status)
 
 if [[ $gear_status =~ "active" ]]; then
@@ -7,8 +12,3 @@ echo "Gear is ok✅"
  else 
 echo "Check Gear node❌" 
 fi
-
-function get_status() {
-    STATUS=$(sudo systemctl status gear | grep -i active | awk '{print $2}')
-    echo "${STATUS}"
-}
