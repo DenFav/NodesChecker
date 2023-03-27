6641e5016b731e01d305c49e6efc5d103eb3d945
Hamster <denrabshit@gmail.com>
2023-03-27 22:23:16 +0300

    Added get status function

--- gear_checker.sh
+++ gear_checker.sh
@@ -2,17 +2,13 @@
 
 gear_status=$(get_status)
 
-shopt -s nocasematch
-
 if [[ $gear_status =~ "active" ]]; then
 echo "Gear is ok✅"
  else 
-echo "Check gear node❌" 
+echo "Check Gear node❌" 
 fi
 
-shopt -u nocasematch
-
 function get_status() {
-    STATUS=$(docker exec -t shardeum-dashboard operator-cli status | grep state | awk '{ print $2 }')
+    STATUS=$(sudo systemctl status gear | grep -i active | awk '{print $2}')
     echo "${STATUS}"
 }
