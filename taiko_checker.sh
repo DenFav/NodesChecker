5d511a68974ee092ba52acc917f274a34851b037
Hamster <denrabshit@gmail.com>
2023-03-31 19:43:08 +0300

    Fixed Taiko checker

--- taiko_checker.sh
+++ taiko_checker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 get_status() {
-    STATUS=$(docker-compose -f $HOME/simple-taiko-node/docker-compose.yml logs --tail=100 | grep -i --color=>
+    STATUS=$(docker-compose -f $HOME/simple-taiko-node/docker-compose.yml logs --tail=100 | grep -i --color=='auto' error | grep -wv -i -e trie | grep -wv -i -e L1_ | grep -wv -i -e L1_ALREADY_PROVEN | grep -wv -i -e 'failed to fetch L2 parent block:')
     echo "${STATUS}"
 }
 
