ff48b8777dd6688ef287c908a1d8cd7a66d4f878
Hamster <denrabshit@gmail.com>
2023-04-05 23:31:49 +0300

    Added trap to cd to the root

--- bundler_checker.sh
+++ bundler_checker.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+function cleanup {
+  cd ~
+}
+
+trap cleanup EXIT
+
 cd $HOME/bundlr/validator-rust
 address=$(cargo run --message-format json --bin wallet-tool show-address --wallet wallet.json | grep address |  jq -r .address)
 balance=$(testnet-cli balance "${address}" 2>&1 | grep $address | awk '{ print $7 }' )
 validator=$(npx @bundlr-network/testnet-cli check RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA "${address}" 2>&1 | grep $address)
-cd ~
 
 if [[ "$balance" -eq "0" && "$validator" == *"is a validator for contract"*  ]]; then
 echo "Bundler looks good✅"
