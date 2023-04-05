c77e849718c1dcfaffe100ae5df21e475f3e0e8c
Hamster <denrabshit@gmail.com>
2023-04-05 23:08:28 +0300

    Added Bundler checker script

---
+++ bundler_checker.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd $HOME/bundlr/validator-rust
+address=$(cargo run --message-format json --bin wallet-tool show-address --wallet wallet.json | grep address |  jq -r .ad>
+balance=$(testnet-cli balance "${address}" 2>&1 | grep $address | awk '{ print $7 }' )
+validator=$(npx @bundlr-network/testnet-cli check RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA "${address}" 2>&1 | grep $a>
+cd ~
+
+if [[ "$balance" -eq "0" && "$validator" == *"is a validator for contract"*  ]]; then
+echo "Bundler looks good✅"
+ else 
+echo "Check Bundler node❌" 
+fi
