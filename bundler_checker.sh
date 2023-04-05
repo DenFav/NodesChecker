bd562d4ed8cb3e45a98a5a631d2786612846ee21
Hamster <denrabshit@gmail.com>
2023-04-05 23:17:44 +0300

    Updated scripts, minor updates

--- bundler_checker.sh
+++ bundler_checker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd $HOME/bundlr/validator-rust
-address=$(cargo run --message-format json --bin wallet-tool show-address --wallet wallet.json | grep address |  jq -r .ad>
+address=$(cargo run --message-format json --bin wallet-tool show-address --wallet wallet.json | grep address |  jq -r .address)
 balance=$(testnet-cli balance "${address}" 2>&1 | grep $address | awk '{ print $7 }' )
-validator=$(npx @bundlr-network/testnet-cli check RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA "${address}" 2>&1 | grep $a>
+validator=$(npx @bundlr-network/testnet-cli check RkinCLBlY4L5GZFv8gCFcrygTyd5Xm91CzKlR6qxhKA "${address}" 2>&1 | grep $address)
 cd ~
 
 if [[ "$balance" -eq "0" && "$validator" == *"is a validator for contract"*  ]]; then
