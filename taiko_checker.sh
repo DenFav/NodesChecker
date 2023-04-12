4e53a78cfd7e32af3aa2862c371882419c594d93
Hamster <denrabshit@gmail.com>
2023-04-12 17:58:35 +0300

    Updated Taiko checker scprit

--- taiko_checker.sh
+++ taiko_checker.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 get_status() {
-    STATUS=$(docker-compose -f $HOME/simple-taiko-node/docker-compose.yml logs --tail=100 | grep -i --color=='auto' error | grep -wv -i -e trie | grep -wv -i -e L1_ | grep -wv -i -e L1_ALREADY_PROVEN | grep -wv -i -e 'failed to fetch L2 parent block:')
+    STATUS=$(docker-compose -f $HOME/simple-taiko-node/docker-compose.yml logs --tail=100 | \
+grep -wv -i -e trie | \
+grep -wv -i -e L1_ | \
+grep -wv -i -e L1_ALREADY_PROVEN | \
+grep -wv -i -e 'failed to fetch L2 parent block:' | \
+grep -wv -i -e 'error="mime: no media type"' | \
+grep -wv -i -e 'connect: connection refused' | \
+grep -wv -i -e 'Failed to clean stale beacon headers' | \
+grep -wv -i -e 'msg="Failed to send usage stats"')
+
     echo "${STATUS}"
 }
 
